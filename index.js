51b13696addf38f10a42c9028224949becd9ea9d
Roberto Mendes <robertompfm@gmail.com>
2024-12-22 17:21:59 -0300

    Phonebook database step 8

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
-const Person = require('./models/person')
+const Person = require('./models/person');
 const app = express()
-
-let persons = []
 
 app.use(express.static('dist'))
 app.use(express.json())
@@ -54,25 +52,18 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    { name, number }, 
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
-
-const validatePerson = (person) => {
-  if (!person.name || !person.number) {
-    return "content missing"
-  }
-}
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 
@@ -80,15 +71,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  
-  const errorMessage = validatePerson(body)
-  
-  if (errorMessage) {
-    return response.status(400).json({
-      error: errorMessage
-    })
-  }
-  
+
   const person = new Person({
     name: body.name,
     number: body.number
@@ -112,7 +95,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
 
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -17,9 +17,22 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-    })
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d{5,}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'Phone number required']
+    }
+})
     
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
